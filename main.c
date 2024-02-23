80bb5f99114207374ba7d3380c96966f06c33234
Pollo <leonardo.brunetti24@gmail.com>
2024-02-23 19:50:37 +0100

    media

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 
 int main(int argc, char const *argv[])
 {
+    int media = 0;
     for (int i = 0; i < 10; i++) {
         printf("ciao %d", i);
+        media += i;
     }
+    printf("media = %d", media / 10);
     return 0;
 }
 
