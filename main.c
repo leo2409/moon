e9746f44a5c8be31ecd2a6ca7e63ea88da8b5507
Leonardo Brunetti <leonardo.brunetti24@gmail.com>
2024-02-23 19:49:13 +0100

    test_func implemebtation

--- main.c
+++ main.c
@@ -4,10 +4,12 @@ int main(int argc, char const *argv[])
 {
     for (int i = 0; i < 10; i++) {
         printf("ciao %d", i);
+        test_func(i);
     }
     return 0;
 }
 
-int test_func() {
-    return 0;
+int test_func(int x) {
+    if (x != 0) return 0;
+    else return x +10;
 }
